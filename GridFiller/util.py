53359dd2e2fd4303b9c18c213580f5acaab572d2
ssangervasi <sebastian.sangervasi@gmail.com>
2015-12-17 16:46:53 -0800

    Got my iterator working

--- GridFiller/util.py
+++ GridFiller/util.py
@@ -1,39 +1,49 @@
+# def loopTest(breadth, depth):
+# 	results = []
+# 	arr = [-1 for i in range(breadth)]
+# 	active = 0
+# 	while active > -1:
+# 		if active >= breadth:
+# 			active -= 1
+# 			continue
+
+# 		arr[active] += 1
+# 		if arr[active] == depth:
+# 			arr[active] = -1
+# 			active -= 1
+# 			continue
+		
+# 		active += 1
+# 		results.append(''.join(str(max(val, 0)) for val in arr))
+
+# 	return results
+	
 def loopTest(breadth, depth):
 	results = []
 	arr = [0 for i in range(breadth)]
-	right = active = first = 0
-	# for BIGLOOP in range(depth ** breadth):
-	while right < breadth:
-		hitBoundary = True
-		for index in range(first, active + 1):
-			if hitBoundary:
-				arr[index] = (arr[index] + 1)
-				hitBoundary = arr[index] >= depth
-			else:
-				break
+	active = 0
+	back = 0
+	while active > -1:
+		if active >= breadth:
+			active -= 1
+			back = 1
+			continue
+
+		arr[active] += back
+		if arr[active] == depth:
+			arr[active] = 0
+			active -= 1
+			back = 1
+			continue
 		
-		if hitBoundary:
-			active += 1
-			arr = [0 if i <= active + 1 else arr[i] for i in range(breadth)]
-			if active < breadth:
-				arr[active] = 1
-
-		if active > right:
-			# print("right", active, right)
-			right += 1
-			active = first
-
-		results.append(''.join(str(val) for val in arr))
+		active += 1
+		back = 0
+		results.append(''.join(str(max(val, 0)) for val in arr))
 
 	return results
 
-testRes = loopTest(3, 2)
-print(testRes)
-print("Actual", len(testRes))
-print("Unique", len(set(testRes)))
-
 def recursionTest(breadth, depth):
-	results = set()
+	results = []
 	recursionHelper(0, depth, [0 for i in range(breadth)], results)
 	return results
 
@@ -41,12 +51,22 @@ def recursionHelper(index, depth, arr, results):
 	if index >= len(arr):
 		return
 	for myDepth in range(depth):
-		nextArr = arr.copy()
+		# nextArr = arr.copy()
+		nextArr = arr
 		nextArr[index] = myDepth
-		results.add(''.join(str(val) for val in nextArr))
+		results.append(''.join(str(val) for val in nextArr))
 		recursionHelper(index + 1, depth, nextArr, results)
 
-testRes = recursionTest(3, 2)
-print(testRes)
-print("Actual", len(testRes))
-print("Unique", len(set(testRes)))
+m = 4
+n = 2
+loopRes = loopTest(m, n)
+print(loopRes)
+print("Actual", len(loopRes))
+print("Unique", len(set(loopRes)))
+
+recRes = recursionTest(m, n)
+print(recRes)
+print("Actual", len(recRes))
+print("Unique", len(set(recRes)))
+
+print("Diff?", set(recRes) - set(loopRes))
